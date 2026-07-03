251a659cc57ed2c75e7290298616c76f857c8ea6
CROWN LICENSE VAULT <crownlicensevault@gmail.com>
2026-07-04 02:59:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require("express");
+const app = express();
+
 console.log("Crown License Vault Bot Running...");
 
 setInterval(() => {
   console.log("Bot is alive...");
 }, 10000);
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
